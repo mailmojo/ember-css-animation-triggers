48eb93339ab5ba759d1926431f69f86dd55fbb3b
Simen Heggestøyl <simen@e5r.no>
2016-04-20 16:56:00 +0200

    Add integration tests for `animate-on-change`
    Summary:
    Add integration tests for the `animate-on-change` component.
    
    Depends on D3916.
    
    Reviewers: fdanielsen, asteinlein
    
    Reviewed By: asteinlein
    
    Differential Revision: http://dev.e5r.no/D3918

--- tests/integration/components/animate-on-change-test.js
+++ tests/integration/components/animate-on-change-test.js
@@ -5,15 +5,13 @@ moduleForComponent('animate-on-change', 'Integration | Component | animate on pr
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('it renders', function (assert) {
+  assert.expect(2);
 
   this.render(hbs`{{animate-on-change}}`);
 
   assert.equal(this.$().text().trim(), '');
 
-  // Template block usage:
   this.render(hbs`
     {{#animate-on-change}}
       template block text
@@ -21,4 +19,34 @@ test('it renders', function(assert) {
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
+});
+
+test('it adds an animation class on value change', function (assert) {
+  assert.expect(2);
+
+  this.set('val', 0);
+
+  this.render(hbs`{{animate-on-change animationClass="foo"
+                    observedValue=val}}`);
+
+  // `val` hasn't changed yet
+  assert.notOk(this.$('> span').hasClass('foo'));
+
+  this.set('val', 1);
+
+  // `val` has changed now; the class should've been added
+  assert.ok(this.$('> span').hasClass('foo'));
+});
+
+test("it doesn't add an animation class when disabled", function (assert) {
+  assert.expect(1);
+
+  this.set('val', 0);
+
+  this.render(hbs`{{animate-on-change animationClass="foo" enabled=false
+                    observedValue=val}}`);
+
+  this.set('val', 1);
+
+  assert.notOk(this.$('> span').hasClass('foo'));
 });
